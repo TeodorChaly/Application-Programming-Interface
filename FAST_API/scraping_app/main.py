933b788036d9d8d8e9fe342915cd51e48fbb94c2
Teodors Chaly <iteo.chaly@gmail.com>
2024-05-06 23:13:20 +0300

    Random crypto function update

--- FAST_API/scraping_app/main.py
+++ FAST_API/scraping_app/main.py
@@ -1,4 +1,5 @@
 import asyncio
+import random
 
 import requests
 import requests as req
@@ -10,21 +11,33 @@ app = FastAPI()
 
 @app.get("/random/")
 async def add_trades():
-    price = await asyncio.gather(async_scraping_random())
-    return {"price": price}
+    info = await asyncio.gather(async_scraping_random())
+    return {"name": info[0][0], "price": info[0][1]}
 
 
 async def async_scraping_random():
-    query = "bitcoin"
-    url = f"https://www.google.com/search?q={query}"
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-        "Accept": "*/*",
-    }
-    response = requests.get(url, headers=headers)
-    html_content = response.text
+    query = ["bitcoin", "ethereum", "dogecoin", "cardano"]
+    word = random.choice(query)
+    try:
 
-    soup = BeautifulSoup(html_content, 'html.parser')
-    bitcoin_price = soup.find('div', class_='card-section PZPZlf').find(class_="pclqee").text
+        url = f"https://www.google.com/search?q={word}"
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+            "Accept": "*/*",
+        }
+        response = requests.get(url, headers=headers)
+        html_content = response.text
 
-    return bitcoin_price
+        soup = BeautifulSoup(html_content, 'html.parser')
+        bitcoin_price = soup.find('div', class_='card-section PZPZlf').find(class_="pclqee").text
+
+        return word, bitcoin_price
+    except Exception as e:
+
+        return word, "Error"
+
+
+@app.get("/random/")
+async def add_trades():
+    info = await asyncio.gather(async_scraping_random())
+    return {"name": info[0][0], "price": info[0][1]}
